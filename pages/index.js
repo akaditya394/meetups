c63314f644318cd9f9390e37ed88e9a7780798b5
Aditya <akaditya394@gmail.com>
2022-03-10 23:58:26 +0530

    meetups shown

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A first meetup",
+    image:
+      "https://cdn.britannica.com/37/189837-050-F0AF383E/New-Delhi-India-War-Memorial-arch-Sir.jpg",
+    address: "some address 5, 12345",
+    description: "a beautiful description",
+  },
+  {
+    id: "m2",
+    title: "A second meetup",
+    image:
+      "https://cdn.britannica.com/37/189837-050-F0AF383E/New-Delhi-India-War-Memorial-arch-Sir.jpg",
+    address: "some address 5, 12345",
+    description: "a second beautiful description",
+  },
+];
+
+function HomePage () {
+  return (<MeetupList meetups={DUMMY_MEETUPS} />);
+};
+
+export default HomePage;
