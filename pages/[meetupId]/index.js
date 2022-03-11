a7e756fcca1ff8ad22c98cf8d763ad2cfbcea702
Aditya <akaditya394@gmail.com>
2022-03-11 22:10:07 +0530

    meetup item added

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+
+const MeetupDetails = () => {
+  return (
+    <Fragment>
+      <img
+        src="https://cdn.britannica.com/37/189837-050-F0AF383E/New-Delhi-India-War-Memorial-arch-Sir.jpg"
+        alt=""
+      />
+      <h1>A first meetup page</h1>
+      <address>some address, 12345</address>
+      <p>meetup description</p>
+    </Fragment>
+  );
+};
+
+export default MeetupDetails;
