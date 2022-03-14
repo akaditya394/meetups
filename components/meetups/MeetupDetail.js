ccd5714c20df479d41507fabb84a580186e7d1c5
Aditya <akaditya394@gmail.com>
2022-03-14 17:24:50 +0530

    css file added for meetup detail

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from "react";
+import classes from "./MeetupDetail.module.css";
 
 const MeetupDetails = (props) => {
   return (
-    <Fragment>
+    <section className={classes.detail}>
       <img src={props.image} alt={props.title} />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
       <p>{props.description}</p>
-    </Fragment>
+    </section>
   );
 };
 
