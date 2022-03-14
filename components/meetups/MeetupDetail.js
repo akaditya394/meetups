bca19cde7896d1d0fe07de91f2b7afc8de210a96
Aditya <akaditya394@gmail.com>
2022-03-14 17:20:48 +0530

    meetup detail page created

---
+++ components/meetups/MeetupDetail.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+const MeetupDetails = (props) => {
+  return (
+    <Fragment>
+      <img src={props.image} alt={props.title} />
+      <h1>{props.title}</h1>
+      <address>{props.address}</address>
+      <p>{props.description}</p>
+    </Fragment>
+  );
+};
+
+export default MeetupDetails;
--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,16 +1,14 @@
 import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 const MeetupDetails = () => {
   return (
-    <Fragment>
-      <img
-        src="https://cdn.britannica.com/37/189837-050-F0AF383E/New-Delhi-India-War-Memorial-arch-Sir.jpg"
-        alt=""
-      />
-      <h1>A first meetup page</h1>
-      <address>some address, 12345</address>
-      <p>meetup description</p>
-    </Fragment>
+    <MeetupDetail
+      image="https://cdn.britannica.com/37/189837-050-F0AF383E/New-Delhi-India-War-Memorial-arch-Sir.jpg"
+      title="First Meetup"
+      address="some address, 12345"
+      description="meetup description"
+    />
   );
 };
 
